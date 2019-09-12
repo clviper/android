08be1492c0a2c2f1b1c5393987d2bf5868171f31
clviper <claudioandre@gmail.com>
2019-09-12 11:58:30 +0100

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -11,7 +11,7 @@ then
   adb shell "iptables -t nat -A OUTPUT -p tcp --dport 443 -j DNAT --to-destination 127.0.0.1:8080"
   adb shell "iptables -t nat -A POSTROUTING -p tcp --dport 80 -j MASQUERADE"
   adb shell "iptables -t nat -A POSTROUTING -p tcp --dport 443 -j MASQUERADE"
-  adb reverse tcp:$1 tcp:8080
+  adb reverse tcp:8080 tcp:$1
   read -n1 -r -p "Android device traffic being redirected to $1 port on localhost. Press any key to stop and clean up device rules ..." key
   adb shell "iptables -t nat -F"
   adb reverse --remove-all
